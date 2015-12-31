024773665f644229b4a3c51ce13eef6419f02ee2
calebmer <calebmeredith8@gmail.com>
2015-12-31 18:43:15 -0500

    Export the event hook class

--- index.js
+++ index.js
@@ -9,5 +9,6 @@ v.VText = require('virtual-dom/vnode/vtext')
 v.Hook = require('./lib/Hook')
 v.Thunk = require('./lib/Thunk')
 v.Widget = require('./lib/Widget')
+v.EventHook = require('./lib/EventHook')
 
 module.exports = v
