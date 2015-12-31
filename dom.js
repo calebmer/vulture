49f06f3bbb249c37fabeec1914004fd3e2f262f1
calebmer <calebmeredith8@gmail.com>
2015-12-31 10:33:40 -0500

    Add named imports for renderers

--- dom.js
+++ dom.js
@@ -1,1 +1,6 @@
-module.exports = require('./lib/renderToDOM')
+var v = require('./lib/renderToDOM')
+
+v.renderToDOM = require('./lib/renderToDOM')
+v.render = require('./lib/renderToDOM')
+
+module.exports = v
--- string.js
+++ string.js
@@ -1,1 +1,6 @@
-module.exports = require('./lib/renderToString')
+var v = require('./lib/renderToString')
+
+v.renderToString = require('./lib/renderToString')
+v.render = require('./lib/renderToString')
+
+module.exports = v
