2a99d1448e7b9e746bd3a6c669f65fbe7e20b4b6
calebmer <calebmeredith8@gmail.com>
2016-01-13 18:41:05 -0500

    Move utils to own export

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 var Vulture = require('./lib/v')
 
 Vulture.v = require('./lib/v')
-Vulture.map = require('./lib/map')
-Vulture.forEach = require('./lib/forEach')
-Vulture.reduce = require('./lib/reduce')
 Vulture.VNode = require('virtual-dom/vnode/vnode')
 Vulture.VText = require('virtual-dom/vnode/vtext')
 Vulture.Hook = require('./lib/Hook')
---
+++ utils.js
@@ -0,0 +1,7 @@
+var Vulture = {}
+
+Vulture.map = require('./lib/map')
+Vulture.forEach = require('./lib/forEach')
+Vulture.reduce = require('./lib/reduce')
+
+module.exports = Vulture
