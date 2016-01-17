662d9ea275090b3c09c596551ed2419e8f53826b
calebmer <calebmeredith8@gmail.com>
2016-01-17 16:04:11 -0500

    Add decorate function

---
+++ component.js
@@ -0,0 +1,6 @@
+var Vulture = {}
+
+Vulture.applyState = require('./lib/applyState')
+Vulture.decorate = require('./lib/decorate')
+
+module.exports = Vulture
---
+++ test/decorate.test.js
@@ -0,0 +1,18 @@
+var assert = require('assert')
+var decorate = require('../lib/decorate')
+
+describe('decorate()', () => {
+  it('will apply decorators in the correct order', () => {
+    function addOne(value) {
+      return value + 1
+    }
+
+    function square(value) {
+      return value * value
+    }
+
+    assert.equal(decorate(addOne, addOne, square)(1), 3)
+    assert.equal(decorate(square, addOne, addOne)(1), 9)
+    assert.equal(decorate(addOne, square, addOne)(1), 5)
+  })
+})
