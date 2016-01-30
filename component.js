54d9080e402f98c6ff9ed2cb8a586117260126e9
calebmer <calebmeredith8@gmail.com>
2016-01-30 11:33:21 -0500

    Move component exports to index

--- component.js
+++ component.js
@@ -1,11 +1,13 @@
+// DEPRECARED
+
 'use strict'
 
 var Vulture = require('./lib/createComponent')
 
-Vulture.makeLazy = require('./lib/makeLazy')
 Vulture.applyState = require('./lib/applyState')
 Vulture.createComponent = require('./lib/createComponent')
 
+Vulture.makeLazy = require('./lib/makeLazy')
 Vulture.decorate = require('./lib/deprecated/decorate')
 Vulture.lazy = require('./lib/deprecated/lazy')
 
--- index.js
+++ index.js
@@ -3,5 +3,8 @@
 var Vulture = require('./lib/v')
 
 Vulture.v = require('./lib/v')
+Vulture.makeLazy = require('./lib/makeLazy')
+Vulture.applyState = require('./lib/applyState')
+Vulture.createComponent = require('./lib/createComponent')
 
 module.exports = Vulture
