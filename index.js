94f9c2d93165c921baa6af1872ed8992773425f5
calebmer <calebmeredith8@gmail.com>
2016-01-30 11:41:24 -0500

    Expose internals

--- index.js
+++ index.js
@@ -3,6 +3,11 @@
 var Vulture = require('./lib/v')
 
 Vulture.v = require('./lib/v')
+Vulture.VNode = require('virtual-dom/vnode/vnode')
+Vulture.VText = require('virtual-dom/vnode/vtext')
+Vulture.Thunk = require('./lib/Thunk')
+Vulture.Widget = require('./lib/Widget')
+Vulture.Hook = require('./lib/Hook')
 Vulture.makeLazy = require('./lib/makeLazy')
 Vulture.applyState = require('./lib/applyState')
 Vulture.createComponent = require('./lib/createComponent')
